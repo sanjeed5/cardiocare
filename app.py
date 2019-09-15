c303a8ab64b9d52fef026f4aab0d0bc53e52bb69
sanjeed5 <msanjeed5@gmail.com>
2019-09-15 14:11:49 +0530

    load pkl in app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import flask
+import pickle
+
+# Use pickle to load in the pre-trained model.
+with open(f'model/weights.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder = 'templates')
 
