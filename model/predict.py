64e3cd3a777c3bea15dc662969c20016ee0e7fff
sanjeed5 <msanjeed5@gmail.com>
2019-09-15 14:10:41 +0530

    changed predict.py

--- model/predict.py
+++ model/predict.py
@@ -1,11 +1,12 @@
 import pickle
 
-def get_probability(input_vec, final_model_file_name):
+# def get_probability(input_vec, final_model_file_name):
+def get_probability(input_vec, clf):
 
-    with open(final_model_file_name, 'rb') as file:
-        clf = pickle.load(file)
+    # with open(final_model_file_name, 'rb') as file:
+    #     clf = pickle.load(file)
         
-    clf = pickle.load(open(final_model_file_name, 'rb'))
+    # clf = pickle.load(open(final_model_file_name, 'rb'))
     prob = clf.predict([input_vec])
     return prob[0]
 
