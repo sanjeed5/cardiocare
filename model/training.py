b003c1b25ab4e6c34a6922d0bbde901461caf54a
Jonathan <jonathanvevance@gmail.com>
2019-10-14 21:19:10 +0530

    removed pp

--- model/training.py
+++ model/training.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import pandas_profiling as pp
 import numpy as np
 from numpy import genfromtxt
 import pickle
@@ -21,40 +19,6 @@ def accuracy(y_true, y_pred):
             numerator += 1
 
     return numerator / N
-
-
-def training_pd(csv_file, final_model_file_name):
-
-    df = pd.read_csv(csv_file)
-    df.columns = (['bp', 'tobaco', 'cholestrol', 'adiposity', 'fam_hist',
-                   'type_a_beh', 'obesity', 'alcohol', 'age', 'Class'])
-
-    x = df.drop('Class', axis = 1)
-    y = df['Class']
-
-    y = y.to_numpy()
-    y = np.array([1 if i == 1 else 0 for i in y])
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                            test_size = 0.2, random_state = 42)
-
-    scaler = MinMaxScaler()
-    scaler.fit_transform(x_train)
-    scaler.transform(x_test)
-
-    clf = RandomForestClassifier(n_estimators = 100, max_depth = 6, random_state = 0)
-    clf.fit(x_train, y_train)
-    y_train_pred = clf.predict(x_train)
-    y_test_pred = clf.predict(x_test)
-
-    results_dict = dict()
-    results_dict['train_accuracy'] = accuracy(y_train, y_train_pred)
-    results_dict['test_accuracy'] = accuracy(y_test, y_test_pred)
-
-    with open(final_model_file_name, 'wb') as file:
-        pickle.dump(clf, file)
-
-    return results_dict
 
 
 def training_np(csv_file, final_model_file_name):
